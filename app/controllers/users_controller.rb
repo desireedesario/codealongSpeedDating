ffce490f273676d2d42c3eafab898959933cf36e
Desiree Desario <desireedesario@gmail.com>
2016-07-26 17:09:30 -0700

    fix users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,23 +2,23 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
 
-    def index
-      @users = User.all
-    end
+  def index
+    @users = User.all
+  end
 
-    def show
-    end
+  def show
+  end
 
-    def create
-      @user = User.new(user_params)
-      @user.user_id = current_user.id
-        if @user.save
-          redirect_to @user, notice: 'Account was successfully created.'
-        else
-          render :new
-        end
+  def create
+    @user = User.new(user_params)
+    @user.user_id = current_user.id
+    if @user.save
+      redirect_to @user, notice: 'Account was successfully created.'
+    else
+      render :new
     end
   end
+
 
 private
   def set_user
